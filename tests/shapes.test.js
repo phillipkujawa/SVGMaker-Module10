8b5821b96d92191051f99cdf6de0365ce93eac0b
phillipkujawa <phillip.kujawa@me.com>
2023-08-08 06:41:05 -0400

    Moves shapes.js to lib folder

--- tests/shapes.test.js
+++
@@ -1,7 +0,0 @@
-const { Triangle, Circle, Square } = require('./shapes');
-
-test('Triangle render with blue color', () => {
-    const shape = new Triangle();
-    shape.setColor("blue");
-    expect(shape.render()).toBe('<polygon points="150,18 244,182 56,182" fill="blue" />');
-});
